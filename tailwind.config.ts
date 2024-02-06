c0b6718790bb93727eebceb52f9c03bad8fd2c9b
halvaradop_ <hernanvid123@gmail.com>
2024-02-06 15:10:39 -0500

    set colors in tailwind.config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,13 +6,33 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'class',
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        alabaster: {
+          DEFAULT: 'hsl(231, 100%, 99%)'
+        },
+        magnolia: {
+          DEFAULT: 'hsl(217, 100%, 97%)'
+        },
+        red: {
+          DEFAULT: 'hsl(354, 84%, 57%)'
+        },
+        blue: {
+          DEFAULT: 'hsl(206, 94%, 87%)',
+          100: 'hsl(213, 96%, 18%)',
+          200: 'hsl(243, 100%, 62%)',
+          300: 'hsl(228, 100%, 84%)'
+        },
+        gray: {
+          DEFAULT: 'hsl(229, 24%, 87%)',
+          100: 'hsl(231, 11%, 63%)'
+        }
+      }
     },
   },
   plugins: [],
